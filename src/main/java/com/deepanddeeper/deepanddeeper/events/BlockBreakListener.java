43ddb3477d071a93bb0ce55b58d9e961b9f3a1a9
matteopolak <matteopolak@hotmail.com>
2023-09-27 19:43:14 -0400

    feat: use red text

--- src/main/java/com/deepanddeeper/deepanddeeper/events/BlockBreakListener.java
+++ src/main/java/com/deepanddeeper/deepanddeeper/events/BlockBreakListener.java
@@ -1,5 +1,7 @@
 package com.deepanddeeper.deepanddeeper.events;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
@@ -7,6 +9,6 @@ import org.bukkit.event.player.PlayerMoveEvent;
 public class BlockBreakListener implements Listener {
 	@EventHandler
 	public void onPlayerMove(PlayerMoveEvent event) {
-		event.getPlayer().sendMessage("you moved");
+		event.getPlayer().sendMessage(Component.text("hello world", TextColor.color(255, 0, 0)));
 	}
 }
