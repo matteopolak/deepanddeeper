46944095870e6c512d90cb0a9adf5a3bcdebb965
matteopolak <matteopolak@hotmail.com>
2023-09-29 18:54:34 -0400

    feat: weapon loading from config

--- src/main/java/com/deepanddeeper/deepanddeeper/DeepAndDeeper.java
+++ src/main/java/com/deepanddeeper/deepanddeeper/DeepAndDeeper.java
@@ -4,21 +4,38 @@ package com.deepanddeeper.deepanddeeper;
 import com.deepanddeeper.deepanddeeper.commands.GetWorldCommand;
 import com.deepanddeeper.deepanddeeper.events.EntityClickListener;
 import com.deepanddeeper.deepanddeeper.events.PlayerJoinListener;
+import com.deepanddeeper.deepanddeeper.weapons.Weapon;
+import com.deepanddeeper.deepanddeeper.weapons.WeaponHolder;
+import com.google.common.base.Enums;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.serialization.ConfigurationSerialization;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginManager;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+
 public final class DeepAndDeeper extends JavaPlugin {
 	private FileConfiguration config = this.getConfig();
 	private Database database;
+
+	public WeaponHolder weaponHolder;
 
 	private void registerListeners() {
 		// Add event listeners here
 
 		Listener[] listeners = {
-			new PlayerJoinListener(),
+			new PlayerJoinListener(this),
 			new EntityClickListener(),
 		};
 
@@ -42,11 +59,16 @@ public final class DeepAndDeeper extends JavaPlugin {
 
 	@Override
 	public void onEnable() {
+		Bukkit.getLogger().log(Level.INFO, "Hello, world!");
+
+		this.saveDefaultConfig();
+
+		this.weaponHolder = new WeaponHolder(((List<Object>) this.getConfig().getList("weapons")).stream()
+			.map(weapon -> Weapon.deserialize((Map<String, Object>) weapon))
+			.toList());
+
 		this.registerCommands();
 		this.registerListeners();
-
-		this.config.addDefault("database-uri", "jdbc:postgresql://localhost/deepanddeeper");
-		this.saveConfig();
 
 		/*try {
 			this.database = new Database(this.config.getString("database-uri"));
--- src/main/java/com/deepanddeeper/deepanddeeper/events/PlayerJoinListener.java
+++ src/main/java/com/deepanddeeper/deepanddeeper/events/PlayerJoinListener.java
@@ -1,5 +1,9 @@
 package com.deepanddeeper.deepanddeeper.events;
 
+import com.deepanddeeper.deepanddeeper.DeepAndDeeper;
+import com.deepanddeeper.deepanddeeper.weapons.Weapon;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
@@ -7,13 +11,44 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.Team;
 
 public class PlayerJoinListener implements Listener {
+	private DeepAndDeeper plugin;
+	private Scoreboard scoreboard = Bukkit.getScoreboardManager().getMainScoreboard();
+	private Team playingTeam;
+
+	public PlayerJoinListener(DeepAndDeeper plugin) {
+		this.plugin = plugin;
+
+		try {
+			this.playingTeam = this.scoreboard.registerNewTeam("playing");
+			this.playingTeam.setOption(Team.Option.NAME_TAG_VISIBILITY, Team.OptionStatus.ALWAYS);
+		} catch (IllegalArgumentException e) {
+			// The team already exists
+			this.playingTeam = this.scoreboard.getTeam("playing");
+		}
+	}
+
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent event) {
 		Player player = event.getPlayer();
 
 		// Teleport the player to the lobby
 		player.teleport(new Location(Bukkit.getWorld("world"), 0.5, 0, 0.5, 0, 0));
+
+		// Remove the player from the "playing" team when they join
+		boolean wasInGame = this.playingTeam.removePlayer(player);
+
+		if (wasInGame) {
+			// send message to player using colour codes, e.g. "&cYou left the game &cuh no!"
+			player.sendMessage(Component.text("You left the game", NamedTextColor.RED));
+
+			// now using colour escape codes
+			player.sendMessage("§cYou left the game §6uh no!");
+		}
+
+		this.playingTeam.addPlayer(player);
 	}
 }
